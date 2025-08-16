2a5e89c70490535451de8de178198a67f81e640a
SamB-C <samson_bc@icloud.com>
2025-08-16 19:55:07 +0100

    Allow user to select set

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const output = document.getElementById('output');
+const select = document.getElementById('select');
+
 
 function createImage(imageLink) {
     const img = document.createElement('img');
@@ -25,4 +27,34 @@ async function getCard() {
         });
 }
 
-getCard();
+async function createSelect() {
+    await fetch('http://localhost:8888/sets/en.json')
+        .then(response => response.text())
+        .then(data => {
+            const sets = JSON.parse(data);
+            sets.forEach(set => {
+                const option = document.createElement('option');
+                option.value = set.id;
+                option.textContent = set.name;
+                select.appendChild(option);
+            })
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+createSelect();
+
+select.addEventListener('change', async () => {
+    output.innerHTML = ''; // Clear previous images
+    const selectedSet = select.value;
+    await fetch(`http://localhost:8888/cards/en/${selectedSet}.json`)
+        .then(response => response.text())
+        .then(data => {
+            createImages(JSON.parse(data));
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+})
