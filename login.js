81cae2bf2f1a39cfbe99171de2f7f9f1b21ffa73
Olly Harding <68117086+olliegoldfish@users.noreply.github.com>
2025-08-20 21:04:34 +0100

    Add login capabilities

---
+++ login.js
@@ -0,0 +1,33 @@
+//get references for text input and button fields
+var username = document.getElementById("username")
+var password = document.getElementById("password")
+var jsonBtn = document.getElementById("submit")
+
+
+//add click event listener, to get data when data is entered
+jsonBtn.addEventListener("click", function(){
+    //store data in JavaScript object
+    var data = {
+        "username":username.value,
+        "password":password.value
+    }
+    console.log("Data to be sent:", data);
+
+    fetch("http://localhost:8888/post/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (response.ok) {
+            localStorage.setItem("username", username.value);
+            window.location.href = "open-pack.html";
+
+        } else {
+            throw new Error("Network response was not ok");
+        }
+    })
+})
+
--- server.js
+++ server.js
@@ -7,50 +7,99 @@ var http = require("http"),
 // Create server
 http.createServer(function(request, response) {
 
-  // Parse the request URL to get the file path
-  var uri = url.parse(request.url).pathname,
-      filename = path.join(process.cwd(), uri);
+  // Login Request
+  if (request.method === "POST") {
+    if (request.url === "/post/login") {
+      let body = '';
 
-  // If file doesn't exist, return 404
-  if (!fs.existsSync(filename)) {
-    response.writeHead(404, {"Content-Type": "text/plain"});
-    response.write("404 Not Found\n");
-    response.end();
+      // Collect data from the request
+      request.on('data', chunk => {
+        body += chunk.toString(); // Convert Buffer to string
+      });
+
+      request.on('end', () => {
+        try {
+          const userData = JSON.parse(body);
+          console.log("Received user data:", userData);
+
+          const usersFileData = fs.readFileSync('users.json', 'utf8');
+          const users = JSON.parse(usersFileData);
+          const userLoggedIn = users.find(u => u.username === userData.username && u.password === userData.password);
+          if (userLoggedIn) {
+            console.log("User logged in successfully:", userLoggedIn.username);
+            response.writeHead(200, { "Content-Type": "application/json" });
+            response.end(JSON.stringify({ message: "Login successful" }));
+
+          } else {
+            console.error("Invalid username or password");
+            response.writeHead(401, { "Content-Type": "application/json" });
+            response.end(JSON.stringify({ error: "Invalid username or password" }));
+            return;
+
+          }
+
+          
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+          response.writeHead(400, { "Content-Type": "application/json" });
+          response.end(JSON.stringify({ error: "Invalid JSON" }));
+        }
+      });
+    } else {
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.end("404 Not Found");
+    }
     return;
+  
   }
 
-  // If the request is for a directory, serve index.html (ie if request is "/")
-  if (fs.statSync(filename).isDirectory()) filename += '/home.html';
+  else if (request.method === "GET") {
+    console.log("GET request for: " + request.url);
+    // Parse the request URL to get the file path
+    var uri = url.parse(request.url).pathname,
+        filename = path.join(process.cwd(), uri);
 
-
-  fs.readFile(filename, "binary", function(err, file) {
-    if(err) {        
-      response.writeHead(500, {"Content-Type": "text/plain"});
-      response.write(err + "\n");
+    // If file doesn't exist, return 404
+    if (!fs.existsSync(filename)) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
       response.end();
       return;
     }
 
-    // Split filename on . to get extension
-    const ext = path.extname(filename);
-    console.log("Serving file: " + filename + " with extension: " + ext);
-    let mimeType = "text/plain";
-    if (ext === ".js") {
-      mimeType = "application/javascript";
-    } else if (ext === ".css") {
-      mimeType = "text/css";
-    } else if (ext === ".html" || ext === ".htm") {
-      mimeType = "text/html";
-    } else if (ext === ".json") {
-      mimeType = "application/json";
-    }
-    console.log("MIME type: " + mimeType);
+    // If the request is for a directory, serve index.html (ie if request is "/")
+    if (fs.statSync(filename).isDirectory()) filename += '/home.html';
 
 
-    response.writeHead(200, { "Content-Type": mimeType });
-    response.write(file, "binary");
-    response.end();
-  });
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      // Split filename on . to get extension
+      const ext = path.extname(filename);
+      //console.log("Serving file: " + filename + " with extension: " + ext);
+      let mimeType = "text/plain";
+      if (ext === ".js") {
+        mimeType = "application/javascript";
+      } else if (ext === ".css") {
+        mimeType = "text/css";
+      } else if (ext === ".html" || ext === ".htm") {
+        mimeType = "text/html";
+      } else if (ext === ".json") {
+        mimeType = "application/json";
+      }
+      //console.log("MIME type: " + mimeType);
+
+
+      response.writeHead(200, { "Content-Type": mimeType });
+      response.write(file, "binary");
+      response.end();
+    });}
+
 }).listen(parseInt(port, 10));
 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
